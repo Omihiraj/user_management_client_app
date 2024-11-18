68a151558748a955994929ceb80ecfbe3be2088e
dckrajapaksha <168925124+dckrajapaksha@users.noreply.github.com>
2024-11-18 21:44:00 +0530

    Change splash screen and add native splash

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
+import 'package:user_management_client_app/home_page.dart';
 import 'package:user_management_client_app/screens/main_screen.dart';
 import 'package:page_transition/page_transition.dart';
-import 'package:user_management_client_app/home_page.dart';
+
 
 
 void main() {
+   
+  
   runApp(const MyApp());
 }
+
+
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -15,12 +21,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-
+      
       debugShowCheckedModeBanner: false,
       home: AnimatedSplashScreen(
-        duration: 4000,
+        duration: 3200,
         splash: Image.asset('assets/splashIcon.gif'),
-        nextScreen: const MainScreen(),
+        nextScreen: const HomePage(),
         splashTransition: SplashTransition.fadeTransition,
         pageTransitionType: PageTransitionType.fade,
         backgroundColor: const Color.fromRGBO(24, 28, 20, 1),
